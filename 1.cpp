5c378202c6123b6957c593a2f49426b5f47a58c0
vyom2527 <vyommachchhar@gmail.com>
2023-05-11 22:54:21 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the 1st value"<<endl;
+    cin>>a;
+    cout<<"Enter the 2nd value"<<endl;
+    cin>>b;
+    int c = a + b;
+    cout<<"Addition is = "<<c;
+    return 0;
+}
